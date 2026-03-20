b7d8cdc44a04b046b04e932d25ead86521828af5
Jmorenoq91 <jmorenoq91@gmail.com>
2026-03-20 08:30:23 -0500

    Update mortage_app.py

--- mortage_app.py
+++ mortage_app.py
@@ -97,7 +97,7 @@ rem_bal = loan_amount
 for i in range(1, int(number_of_payments) + 1):
     int_exp = rem_bal * monthly_rate
     cap_amort = monthly_payment - int_exp
-    realized_payment = min(monthly_payment * 1.0, rem_bal + int_exp)
+    realized_payment = min(monthly_payment * 1.2, rem_bal + int_exp)
     rem_bal -= cap_amort
     schedule.append({
         "Month": i,
