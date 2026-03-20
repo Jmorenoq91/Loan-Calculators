78eab6b21ac58c1a0b74e0cd9728061034567608
Jmorenoq91 <jmorenoq91@gmail.com>
2026-03-20 09:16:41 -0500

    Update mortage_app.py

--- mortage_app.py
+++ mortage_app.py
@@ -61,9 +61,10 @@ with st.sidebar:
         down_payment_pct = st.slider("Down Payment %", 10, 90, 20)
         annual_interest_rate = st.number_input("Annual Interest Rate (%)", min_value=0.0, value=5.0, step=0.1)
         loan_term_years = st.slider("Loan Term (Years)", 5, 30, 20, step=5)
-        extra_payment = st.number_input("Extra Payment ($)", min_value=0.0, value=0, step=10.0)
+        #extra_payment = st.number_input("Extra Payment ($)", min_value=0.0, value=0, step=10.0)
 
 # --- Financial Calculations ---
+extra_payment = 0
 down_payment_amt = prop_price * (down_payment_pct / 100)
 loan_amount = prop_price - down_payment_amt
 monthly_rate = (annual_interest_rate / 100) / 12
